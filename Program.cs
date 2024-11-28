e0b0cdf31bf2ab25812e65942843243f9fbe322b
Stefan <Stefan@DESKTOP-P1OADI2>
2024-11-28 14:03:03 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment_2._1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Call Methods listed below
+
+            Console.ReadKey();
+        }
+
+        public class CNum
+        {
+            public double x; 
+            public double y;
+
+             public static double Addition(double x, double y)
+            {
+                return x + y;
+            }
+
+            public static double Addition(double x, double y, double z)
+            {
+                return x + y + z; 
+            }
+
+            public static double Multiplication(double x, double y)
+            { 
+                return x * y;
+            }
+
+            public static double Multiplication(double x, double y, double z)
+            {
+                return x * y + y * z;
+            }
+        }
+
+
+    }
+}
